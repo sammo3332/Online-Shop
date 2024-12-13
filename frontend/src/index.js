f40681ee7b3a0c5d588d52d950a490829acc293a
Houssam <houssamtaleb009@gmail.com>
2024-12-13 15:02:02 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,28 @@
+// Importiere Bootstrap für Styling
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importiere die notwendigen React-Bibliotheken
 import React from 'react';
-import   ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+
+// Importiere benutzerdefinierte CSS-Datei für zusätzliche Stile
 import './index.css';
-  import App from './App';
+
+// Importiere die Hauptkomponente der Anwendung
+import App from './App';
+
+// Importiere die Funktion für Web-Vitals (optional, für Performance-Analysen)
 import reportWebVitals from './reportWebVitals';
 
+// Initialisiere das Root-Element der React-Anwendung
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render die Anwendung in das Root-Element
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-   
+// Starte die Web-Vitals-Analyse (optional)
 reportWebVitals();
